8f13066d486a43a635a8c2e8d0bc12975c785357
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-10-06 23:52:49 +0900

    styling page news

--- components/HomePage/NewsList.js
+++ components/HomePage/NewsList.js
@@ -1,21 +1,24 @@
 import styles from "./NewsList.module.scss";
-import { RiSaveLine } from "react-icons/fa";
+import { AiOutlineSave } from "react-icons/ai";
+import { IconContext } from "react-icons";
 
-function NewsList({ asahiData, user }) {
+function NewsList({ newsData, user }) {
   return (
     <div className={styles.item__title}>
-      {asahiData.map((item, index) => {
+      {newsData.map((item, index) => {
         return (
           <div key={index}>
-            <div>
-              <a href={item.href} target="_blank" rel="noopener noreferrer">
-                <p>{item.title}</p>
-                <p>{item.time}</p>
-              </a>
-            </div>
-            <div>
-              <RiSaveLine />
-            </div>
+            <a href={item.href} target="_blank" rel="noopener noreferrer">
+              <p>{item.title}</p>
+              <p>{item.time}</p>
+            </a>
+            <p>
+              <IconContext.Provider
+                value={{ color: "black", size: "15px", cursor: "pointer" }}
+              >
+                <AiOutlineSave />
+              </IconContext.Provider>
+            </p>
           </div>
         );
       })}
