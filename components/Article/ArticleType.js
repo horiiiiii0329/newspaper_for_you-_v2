d00833286a04b55a945db492dade53d2baf85e70
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-10-09 17:19:37 +0900

    complete styling a article right sec

--- components/Article/ArticleType.js
+++ components/Article/ArticleType.js
@@ -1,5 +1,14 @@
+import styles from "./ArticleType.module.scss";
+
 function ArticleType() {
-  return <div>a</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <h3>スクラップブック</h3>
+      </div>
+      <div className={styles.categories}></div>
+    </div>
+  );
 }
 
 export default ArticleType;
