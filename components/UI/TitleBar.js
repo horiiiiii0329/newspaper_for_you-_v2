e8bf80d551e3bd7847ddde06b87b4a50c3b322dc
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-10-24 13:00:29 +0900

    implement an hamberger menu

--- components/UI/TitleBar.js
+++ components/UI/TitleBar.js
@@ -1,22 +1,40 @@
 import { LightBulbIcon } from "@heroicons/react/outline";
 import { MenuIcon } from "@heroicons/react/outline";
 import styles from "./TitleBar.module.scss";
+import { useState } from "react";
 
 function TitleBar() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <header className={styles.Header}>
-      <div>
-        <LightBulbIcon
-          style={{ width: "80px", height: "80px", cursor: "pointer" }}
-          onClick={() => {}}
-        />
-      </div>
-      <div>
-        <MenuIcon
-          style={{ width: "80px", height: "80px", cursor: "pointer" }}
-        />
-      </div>
-    </header>
+    <>
+      <header className={styles.Header}>
+        <div>
+          <LightBulbIcon
+            style={{ width: "50px", height: "50px", cursor: "pointer" }}
+            onClick={() => {}}
+          />
+        </div>
+        <div onClick={() => setShowModal(!showModal)}>
+          <MenuIcon
+            style={{ width: "40px", height: "40px", cursor: "pointer" }}
+          />
+        </div>
+      </header>
+      {showModal && (
+        <div className={styles.menu}>
+          <ul>
+            <ul>
+              <li>サインイン</li>
+            </ul>
+            <li>クリップした記事</li>
+            <li>みんなの記事</li>
+            <li>作成</li>
+            <li>個人</li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
 
