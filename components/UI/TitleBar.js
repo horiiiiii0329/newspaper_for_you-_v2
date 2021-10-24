429cc9a24c3dd57b203f98cf44af70ad32c082bb
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-10-24 12:23:54 +0900

    add an smartphone heaeder

--- components/UI/TitleBar.js
+++ components/UI/TitleBar.js
@@ -1,5 +1,23 @@
+import { LightBulbIcon } from "@heroicons/react/outline";
+import { MenuIcon } from "@heroicons/react/outline";
+import styles from "./TitleBar.module.scss";
+
 function TitleBar() {
-  return <div></div>;
+  return (
+    <header className={styles.Header}>
+      <div>
+        <LightBulbIcon
+          style={{ width: "80px", height: "80px", cursor: "pointer" }}
+          onClick={() => {}}
+        />
+      </div>
+      <div>
+        <MenuIcon
+          style={{ width: "80px", height: "80px", cursor: "pointer" }}
+        />
+      </div>
+    </header>
+  );
 }
 
 export default TitleBar;
--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,14 @@ import { supabase } from "../api";
 import AuthUser from "../components/Profile/AuthUser";
 import Header from "../components/Header/Header";
 import { AppWrapperProvider } from "../context/state";
+import TitleBar from "../components/UI/TitleBar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <TitleBar />
       <Header />
+
       <AppWrapperProvider>
         <Component {...pageProps} />
       </AppWrapperProvider>
