6abd67cfa70f939d9248a1efbc8df107dfdfc928
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-11-12 22:55:15 +0900

    transfere ui to ts

--- components/Layout/SectionLayout.tsx
+++ components/Layout/SectionLayout.tsx
@@ -1,6 +1,8 @@
+import React from "react";
 import styles from "./SectionLayout.module.scss";
+import { ReactNode } from "react";
 
-function Layout({ children }) {
+function Layout({ children }: { children: ReactNode }) {
   return <section className={styles.wrapper}>{children}</section>;
 }
 
