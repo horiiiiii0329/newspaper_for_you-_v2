9c5a3c25d7a978ee3334342df147de1c910963aa
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-11-13 17:10:18 +0900

    done styled header

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,23 @@
 module.exports = {
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
   stories: [
     "../components/**/*.stories.mdx",
     "../**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-typescript",
+  ],
 
   webpackFinal: async (baseConfig) => {
     baseConfig.module.rules.push({
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,16 +5,25 @@ import {
   CloudIcon,
   PencilIcon,
   UserIcon,
+  ChevronRightIcon,
+  ChevronLeftIcon,
 } from "@heroicons/react/outline";
 import { useRouter } from "next/router";
 import styles from "./Header.module.scss";
+import { useState } from "react";
+
+const navBarIconStyle = { width: "30px", height: "30px", cursor: "pointer" };
 
 function Header() {
+  const [isExpand, setIsExpand] = useState(false);
   const router = useRouter();
 
   return (
-    <header className={styles.Header}>
-      <div>
+    <header
+      className={styles.Header}
+      style={{ width: isExpand ? "200px" : "80px" }}
+    >
+      <div className={styles.Icon}>
         <LightBulbIcon
           style={{ width: "50px", height: "50px", cursor: "pointer" }}
           onClick={() => {
@@ -23,8 +32,87 @@ function Header() {
           }}
         />
       </div>
-      <nav></nav>
-      <div></div>
+      <nav>
+        <ul>
+          <li>
+            <span>
+              <NewspaperIcon style={navBarIconStyle} />
+            </span>
+            <span
+              style={{
+                opacity: isExpand ? "1" : "0",
+              }}
+            >
+              ホーム
+            </span>
+          </li>
+          <li>
+            <span>
+              <ScissorsIcon style={navBarIconStyle} />
+            </span>
+            <span
+              style={{
+                opacity: isExpand ? "1" : "0",
+              }}
+            >
+              クリップした記事
+            </span>
+          </li>
+          <li>
+            <span>
+              <CloudIcon style={navBarIconStyle} />
+            </span>
+            <span
+              style={{
+                opacity: isExpand ? "1" : "0",
+              }}
+            >
+              みんなの記事
+            </span>
+          </li>
+          <li>
+            <span>
+              <PencilIcon style={navBarIconStyle} />
+            </span>
+            <span
+              style={{
+                opacity: isExpand ? "1" : "0",
+              }}
+            >
+              記事の作成
+            </span>
+          </li>
+          <li>
+            <span>
+              <UserIcon style={navBarIconStyle} />
+            </span>
+            <span
+              style={{
+                opacity: isExpand ? "1" : "0",
+              }}
+            >
+              プロフィール
+            </span>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        {!isExpand ? (
+          <ChevronRightIcon
+            style={{ width: "20px", height: "20px", cursor: "pointer" }}
+            onClick={() => {
+              setIsExpand(!isExpand);
+            }}
+          />
+        ) : (
+          <ChevronLeftIcon
+            style={{ width: "20px", height: "20px", cursor: "pointer" }}
+            onClick={() => {
+              setIsExpand(!isExpand);
+            }}
+          />
+        )}
+      </div>
     </header>
   );
 }
