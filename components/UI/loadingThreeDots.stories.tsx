2ac3bc1dce5ef3af2e648205ec21cf518735a8f8
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-11-13 13:03:02 +0900

    complete three dots loading components

---
+++ components/UI/loadingThreeDots.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import LoadingThreeDots from "./loadingThreeDots";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "Components/loadingThreeDots",
+  component: LoadingThreeDots,
+};
+
+const Template = () => <LoadingThreeDots />;
+
+export const Default = Template.bind({});
